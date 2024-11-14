238cb209ec7cf5d108d0b4ff861f668518003aed
StephanieAgatha <ellizavad@gmail.com>
2024-11-14 07:21:05 +0700

    feat: change ping url(again)

--- main.go
+++ main.go
@@ -292,7 +292,7 @@ func readLines(filename string) ([]string, error) {
 func main() {
 	config := Config{
 		SessionURL: "http://18.136.143.169/api/auth/session",
-		PingURL:    "https://nw.nodepay.org/api/network/ping",
+		PingURL:    "https://52.77.10.116/api/network/ping",
 		IPCheckURL: "https://ipinfo.io/json",
 	}
 
